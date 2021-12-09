7f1c6b5c76be61c01de9af682e3b0b2777edf5ad
ZhiyanovAndrey <aazhiyanov@gmail.com>
2021-12-09 05:40:37 +0300

    Добавьте файлы проекта.

---
+++ Lab6_Strings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_Strings
+{
+    class Program
+    {
+        /*Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом.
+         * Знаки препинания не используются. Найти самое длинное слово в строке. */
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            string[] stringArray = str.Split();
+            string longestWord = "";
+            int max = 0;
+            foreach (string s in stringArray)
+            {
+                if (s.Length > max) max = s.Length;
+                if (s.Length == max) longestWord = s.Substring(0);
+
+            }
+
+
+
+            Console.WriteLine("Самое длинное слово: " + longestWord +
+            "\nКоличество символов: " + max);
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Palindrome/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*2. Ввести с клавиатуры предложение. Предложение представляет собой слова, разделенные пробелом. 
+        * Знаки препинания не используются. Составить программу, оределяющую является ли строка палиндромом 
+        * без учёта пробелов и регистра (пример палиндрома – «А роза упала на лапу Азора»)*/
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+
+            //string resultString="";
+            //int max = 0;
+            bool Palindrome = true;
+
+            string delSpaces = str.Replace(" ", "");
+            string resultString = delSpaces.ToUpper();
+
+            char[] s = resultString.ToCharArray();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != s.Length - i) Palindrome = false;
+
+                Console.WriteLine(s[i]);
+            }
+
+
+
+            Console.WriteLine(Palindrome);
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
